1c5004f9c48d240e9714d733e665ddc95a203093
Masud Rana <-g>
2024-08-14 00:26:28 +0600

    Implement website layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "@/app/_styles/global.css";
 import { Josefin_Sans } from "next/font/google";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -20,14 +21,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}
+        className={`bg-primary-950 text-primary-100 min-h-screen flex flex-col ${josefin.className}`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-
-        <div>{children}</div>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import Header from "@/app/_components/Header";
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex items-center justify-center bg-yellow-200"></div>
+    <div className="w-full h-full flex items-center justify-center bg-yellow-200">
+      THIS IS THE HOME PAGE
+    </div>
   );
 }
