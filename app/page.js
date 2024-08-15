af10c5cf84a2c7ee597c3100f1cf54ee835a38df
Masud Rana <-g>
2024-08-15 23:31:56 +0600

    Design home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,29 @@
 import Image from "next/image";
-import Header from "@/app/_components/Header";
+import bg from "../public/bg.png";
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex items-center justify-center bg-yellow-200">
-      THIS IS THE HOME PAGE
+    <div className="mt-24">
+      <Image
+        fill
+        src={bg}
+        quality={80}
+        placeholder="blur"
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
     </div>
   );
 }
