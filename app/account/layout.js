0857b74c207872f8e9cb1a1ee0ff860f09143575
Masud Rana <-g>
2024-08-19 23:17:58 +0600

    Added supabase

--- app/account/layout.js
+++ app/account/layout.js
@@ -4,7 +4,7 @@ export default function AccountLayout({ children }) {
   return (
     <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
       <SideNavigation />
-      <div>{children}</div>
+      <div className="py-1">{children}</div>
     </div>
   );
 }
