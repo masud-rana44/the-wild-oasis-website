69881d67966d5edbc578f1f5dd89c6f3e8fbd595
Masud Rana <-g>
2024-10-09 01:08:20 +0600

    Implement authentication using auth.js

--- app/_components/Reservations.js
+++ app/_components/Reservations.js
@@ -1,5 +1,7 @@
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
 async function Reservations({ cabin }) {
@@ -8,11 +10,16 @@ async function Reservations({ cabin }) {
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-1 min-h-[800px] border border-primary-800">
       <DateSelector cabin={cabin} settings={settings} />
-      <ReservationForm />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <div>Account Page</div>;
+import { auth } from "../_lib/auth";
+
+export default async function Page() {
+  const session = await auth();
+  const firstName = session?.user.name.split(" ")[0];
+
+  return (
+    <div>
+      <h1>Welcome, {firstName}!</h1>
+    </div>
+  );
 }
